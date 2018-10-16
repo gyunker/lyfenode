a64a773b8ea81df5f5b6c079a6c749d4b396202a
Shally Garg <garg.shally05@gmail.com>
2018-10-15 19:32:10 -0700

    Adding Profile model and route

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,10 +9,25 @@ const User = require("../../models/User");
 //@access Public
 router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 
+//Register user:
+
 //@route GET api/users/register
 //@desc Register user
 //@access Public
-router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email });
-});
+
+// router.post("/register", (req, res) => {
+//     User.findOne({ email: req.body.email })
+//     .then(user => {
+//         if(user) {
+//             return res.status(400).json({email: "Email already exists"});
+//         } else{
+//             const newUser = new User({
+//                 name: req.body.name,
+//                 email: req.body.email,
+//                 password: req.body.password
+//             });
+//         }
+//     })
+//   });
+
 module.exports = router;
