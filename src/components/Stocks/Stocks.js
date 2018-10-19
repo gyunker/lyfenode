d58ca6fdc830f347908dcb26bff19a1ded47cf37
Grant Yunker <grantyunker@gmail.com>
2018-10-18 21:57:52 -0700

    stocks link fix

--- src/components/Stocks/Stocks.js
+++ src/components/Stocks/Stocks.js
@@ -4,7 +4,7 @@ import "./stocks.css";
 const StockRow = ({ symbol, latestPrice, change, changePercent }) => {
   return (
     <div className="row stock-row">
-      <div className="col-sm-3"><a href={"https://iextrading.com/apps/stocks/"+symbol} target="_blank">{symbol}</a></div>
+      <div className="col-sm-3"><a href={"https://iextrading.com/apps/stocks/"+symbol} target="_blank" rel="noopener noreferrer">{symbol}</a></div>
       <div className="col-sm-3">{latestPrice.toFixed(2)}</div>
       <div className={"col-sm-3" + ((change < 0) ? ' negative' : ' positive')}>{change.toFixed(2)}</div>
       <div className={"col-sm-3" + ((changePercent < 0) ? ' negative' : ' positive')}>{(changePercent * 100).toFixed(2)}</div>
