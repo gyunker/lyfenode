43ae3d3665aae187b6491e73beb1a1930186ee34
Shally Garg <garg.shally05@gmail.com>
2018-10-20 13:19:58 -0700

    Login passport and jwt backend implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 //Load User model
 const User = require("../../models/User");
@@ -47,5 +50,60 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+//@route GET api/users/login
+//@desc Login user / returning JWT token
+//@access Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //User Matched
+        //Create jwt payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        //Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
+//@route GET api/users/login
+//@desc Login user / returning JWT token
+//@access Public
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
