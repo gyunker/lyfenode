cf7be5fd9dd0b204aae63938de12f59b8840af10
NHolly <nickrhollenbeck@gmail.com>
2018-10-22 19:01:00 -0700

    Added Proxy Setting In Client Package.json

--- client/src/components/Wrapper/Wrapper.js
+++ client/src/components/Wrapper/Wrapper.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Wrapper extends Component {
+  componentDidMount() {
+    axios
+      .get("/test")
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err));
+  }
   render() {
     return <div className="container-fluid p-0">{this.props.children}</div>;
   }
