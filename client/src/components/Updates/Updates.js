a1182989dd2ea33fcbc07b33ffbfaeee27ac7ba5
Dan Linehan <wagist@gmail.com>
2018-10-26 04:24:08 +0200

    Add health (#25)
    * Add chart and router as dependency
    
    * Setup health API
    
    * Add CORS to server
    
    * Add health chart and updates form
    
    * Move healthchart to new directory
    
    * Fix merge
    
    * Update package.json

--- client/src/components/Updates/Updates.js
+++ client/src/components/Updates/Updates.js
@@ -2,18 +2,82 @@ import React from "react";
 import "./updates.css";
 
 class Updates extends React.Component {
-  state = {
-    first_name: "",
-    last_name: "",
-    email_address: ""
-  };
 
-  handleFormInput = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+    state = {
+        workoutHours: '',
+        weight: '',
+        drinks: '',
+        sleepWell: '',
+        sleepHours: ''
+    };
+
+    handleFormInput = (event) => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = (event) => {
+        const state = { ...this.state }
+
+        this.setState({
+            workoutHours: '',
+            weight: '',
+            drinks: '',
+            sleepWell: '',
+            sleepHours: ''
+        });
+
+        event.preventDefault();
+        fetch('/api/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: 'test@test.com', password: 'testtest' })
+        }).then(res => res.json()).then(({ token }) => {
+            return fetch('/api/health/submit', {
+                method: 'POST',
+                headers: { Authorization: token, 'Content-Type': 'application/json' },
+                body: JSON.stringify(state)
+            }).then(res => res.json())
+        }).then(console.log).catch(console.log)
+    };
+
+
+    render() {
+        return (
+            <div id="updates">
+                <form name="application">
+                    <fieldset>
+                        <h6> Updates Needed </h6>
+                        <div id="question">
+                            <label>How many hours did you workout last week? </label>
+                            <input type="number" onChange={this.handleFormInput} name="workoutHours" placeholder="3" value={this.state.workoutHours} />
+                        </div>
+                        <div id="question">
+                            <label>What is your weight today? </label>
+                            <input type="number"  onChange={this.handleFormInput} name="weight" placeholder="175" value={this.state.weight} />
+                        </div>
+                        <div id="question">
+                            <label>How many drinks did you have last week? </label>
+                            <input type="number"  onChange={this.handleFormInput} name="drinks" placeholder="8" value={this.state.drinks} />
+                        </div>
+                        <div id="question">
+                            <label>Did you sleep well last night? </label>
+                            <input type="text" onChange={this.handleFormInput} name="sleepWell" placeholder="Yes" value={this.state.sleepWell} />
+                        </div>
+                        <div id="question">
+                            <label>How many hours did you sleep last night? </label>
+                            <input type="number"  onChange={this.handleFormInput} name="sleepHours" placeholder="8" value={this.state.sleepHours} />
+                        </div>
+                    </fieldset>
+                    <button className="submit" onClick={this.handleFormSubmit}>Submit</button>
+                </form>
+            </div>
+        );
+    };
+};
+
 
   handleFormSubmit = event => {
     event.preventDefault();
--- server.js
+++ server.js
@@ -14,6 +14,18 @@ const port = process.env.PORT || 3001;
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+})
+
 // Serve Static Assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
