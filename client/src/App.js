79762414ae7fdee20d34f8c8098d07c11c5d9ed7
Shally Garg <garg.shally05@gmail.com>
2018-10-27 13:15:38 -0700

    Navbar cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 
 import Wrapper from "./components/Wrapper/Wrapper";
 import Navbar from "./components/Navbar/Navbar";
@@ -11,6 +14,16 @@ import Landing from "./components/Landing/Landing";
 import Dashboard from "./components/Dashboard/Dashboard";
 import HealthChart from "./components/HealthChart/HealthChart";
 import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 const App = () => (
   <Provider store={store}>
