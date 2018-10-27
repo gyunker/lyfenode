7dc3036b7559e7a4a214c628bb016b02403870c5
Shally Garg <garg.shally05@gmail.com>
2018-10-27 10:32:15 -0700

    Adding redux functionaliy for signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Wrapper from "./components/Wrapper/Wrapper";
 import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/auth/Register";
@@ -9,17 +12,19 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <Wrapper>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <div className="container">
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-      <Route exact path="/dashboard" component={Dashboard} />
-    </Wrapper>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Wrapper>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Wrapper>
+    </Router>
+  </Provider>
 );
 
 export default App;
