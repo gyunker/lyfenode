b4da8ae3bc9e4effdea497a35d3e7ad2e101c607
Grant Yunker <grantyunker@gmail.com>
2018-10-31 23:10:29 -0700

    axios in keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: "mongodb://shally:shally123@ds037468.mlab.com:37468/lyfenode",
-  secretOrKey: "secret"
+  secretOrKey: "secret",
+  axios: {
+    client_id: "5bb8f3f1f17fb3001118dde6",
+    secret: "318a49163bd2f83de0d2b2c166be02"
+  }
 };
